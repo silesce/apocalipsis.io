a71eea308d17661aaf17c584f82d112a03eee33b
silesce <lmarcelo.rodriguez@gmail.com>
2016-04-14 16:43:33 -0300

    Update pagefeedwebconnect.js
    id, homepage

--- fwdc/js/pagefeedwebconnect.js
+++ fwdc/js/pagefeedwebconnect.js
@@ -1,7 +1,7 @@
 
-        var APP_ID = '<YOUR APP ID>'; //Facebook APP ID
+        var APP_ID = '<274690019535368>'; //Facebook APP ID
         var LIMIT = 250; //Number of records to bring back at any time
-        var BASEURL = '<YOUR SERVER NAME>/facebooksearch/' //base url of virtual directory
+        var BASEURL = 'http://silesce.github.io/fwdc/facebooksearch/' //base url of virtual directory
 
         window.fbAsyncInit = function() {
             FB.init({
