7f24c04d0a3032f10af9fabaef4bf9d1f6cab041
silesce <lmarcelo.rodriguez@gmail.com>
2017-01-09 15:52:21 -0300

    Change date type

--- YouTube/youtube.js
+++ YouTube/youtube.js
@@ -128,8 +128,8 @@ myConnector.init = function() {
 
 
 myConnector.getColumnHeaders = function() {
-    fieldNames = ["Video ID","Channel ID","month","insightTrafficSourceType","Views","Likes","Dislikes","Comments","Shares","Estimated Minutes Watched","Avg View Duration (s)", "subscribersGained"];
-    var fieldTypes = ["string","string","string","string","int","int","int","int","int","int","int","int"];
+    fieldNames = ["Video ID","Channel ID","month","insightTrafficSourceDetail","Views","Likes","Dislikes","Comments","Shares","Estimated Minutes Watched","Avg View Duration (s)","subscribersGained"];
+    var fieldTypes = ["string","string","DateType","string","int","int","int","int","int","int","int","int"];
     tableau.headersCallback(fieldNames, fieldTypes);
 };
 
